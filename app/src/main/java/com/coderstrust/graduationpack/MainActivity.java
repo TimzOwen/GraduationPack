e631660c7612b3244efbe49f397601989d9f7dfe
GEORGES <ktimothy@kabarak.ac.ke>
2020-09-26 22:55:52 -0700

    toast

--- app/src/main/java/com/coderstrust/graduationpack/MainActivity.java
+++ app/src/main/java/com/coderstrust/graduationpack/MainActivity.java
@@ -3,12 +3,39 @@ package com.coderstrust.graduationpack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // step 1
+        Button btnGradauate = findViewById(R.id.button2);
+        Button btnIntern = findViewById(R.id.button);
+
+        //step 2
+        btnGradauate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //step 3
+                Toast.makeText(MainActivity.this,"Hello new Graduate",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnIntern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Intern you smart!!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+}}
